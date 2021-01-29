abca77a4eb0ec5ed8519d506bb468bde70b304be
KiddKool <46791472+KiddKool@users.noreply.github.com>
2021-01-28 19:07:25 -0600

    Initial commit

---
+++ Assets/PlayName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayName : MonoBehaviour
+{
+    public InputField NamePlayer;
+    public string myNamePlayer;
+    public Text ShowName;
+
+    public void DisplayName()
+    {
+        myNamePlayer = NamePlayer.text;
+        ShowName.text = myNamePlayer;
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GameProgrammingKidd
+ 
